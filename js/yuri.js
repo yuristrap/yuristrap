39a2ac138f08750d2be2a98ad74814b8a073118a
YeChan Kang <kyechan99@gmail.com>
2020-09-20 10:45:47 +0000

    ADD : modal skeleton

--- js/yuri.js
+++ js/yuri.js
@@ -1,12 +1,30 @@
 $(function(){
     $('body').on('click', '[data-toggle="collapse"]', function(e){
-      e.preventDefault();
-      var target_selector = $(this).data('target');
+		e.preventDefault();
+		var collapseTarget = $(this).data('target');
 
-      if (!$(target_selector).hasClass('show')) {
-        $(target_selector).addClass('show');
-      } else {
-        $(target_selector).removeClass('show');
-      }
+		if (!$(collapseTarget).hasClass('show'))
+			$(collapseTarget).addClass('show');
+		else
+			$(collapseTarget).removeClass('show');
     });
-  });
+    $('body').on('click', '[data-toggle="modal"]', function(e){
+		e.preventDefault();
+		var modalTarget = $(this).data('target');
+		
+		if (!$(modalTarget).hasClass('addModalCloseEvent') && !$(modalTarget).hasClass('bg-disabled')) {
+			$(modalTarget).addClass('addModalCloseEvent');
+			for (var i = 0 ; i < $(modalTarget).length; i++) {
+				$(modalTarget)[i].addEventListener('click' , function(e) {
+				   if (e.target == this && $(this).hasClass('show'))
+						$(this).removeClass('show');
+				});
+			}
+		}		
+		
+		if (!$(modalTarget).hasClass('show'))
+			$(modalTarget).addClass('show');
+		else
+			$(modalTarget).removeClass('show');
+    });
+});
